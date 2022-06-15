ea91c5967c399d2d4a08ce235f324701c19f628c
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-06-14 21:25:05 -0300

    Create Ejercicio1Bucle.js

---
+++ JavaScritp/Ejercicio1Bucle.js
@@ -0,0 +1,24 @@
+var total=10;
+for(i=10;i>0;i--){
+    total= total*i;
+}
+console.log(total)
+
+var tot=10;
+var i=1;
+while(i<=10){
+    tot=tot*i;
+    i++;
+}
+console.log(tot)
+
+var t=10
+var i=1;
+while(i<=10){
+    t=t*i;
+    i++;
+    if(t>1300){
+        break;
+    }
+}
+console.log(t)
