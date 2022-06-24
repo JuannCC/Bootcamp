2678dcb68491b231e09a0d5243168dda47403b1e
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-06-24 19:44:39 -0300

    Create index.js

---
+++ JavaScritp/ESLint/index.js
@@ -0,0 +1,11 @@
+var nombre= "Juan Cruz";
+var apellido='Crotta';
+var estudiante=nombre+` `+apellido;
+var estudianteMayus=estudiante.toUpperCase();
+var estudianteMinus=estudiante.toLowerCase();
+var tamano=estudiante.length;
+var letra=estudiante[0];
+var letra2=apellido[apellido.length-1];
+var recorte=estudiante.replace(/\s+/g, "").trim();
+var bool=estudiante.includes("Juan Cruz")
+/*eslint-disable-next-line*/
