d695437a547f6121981832dd579e2a091c2f3f8b
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-07-11 07:50:41 -0300

    Create SmartDevice.java

---
+++ Java/objeto/SmartDevice.java
@@ -0,0 +1,46 @@
+public class SmartDevice {
+    String marca;
+    String tipo;
+    int anio;
+    public SmartDevice() {
+    }
+
+    public SmartDevice(String marca,String tipo,int anio) {
+        this.marca=marca;
+        this.tipo=tipo;
+        this.anio=anio;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartDevice{" +
+                "marca='" + marca + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", anio=" + anio +
+                '}';
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+}
