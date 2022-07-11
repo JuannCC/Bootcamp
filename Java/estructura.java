a68ecb719cd8bf220f86f45c4f06a769fa60d5e1
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-07-10 22:04:11 -0300

    Create estructura.java

---
+++ Java/estructura.java
@@ -0,0 +1,21 @@
+package javaapplication4;
+
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        String[] vector= {"Juan","Pedro","Vale"};
+        String con;
+        String conC="";
+        //for(String nombres : vector){
+          //  System.out.println(nombres);
+          for (int i = 0; i < vector.length; i++) {
+            String nombres;
+            con=vector[i];
+            conC=con + " " + conC;
+            
+            
+    }
+         System.out.println(conC);
+
+    }
+}
