34f71d0797d965b1b155e04451a7d4872b1f65fb
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-07-11 07:51:06 -0300

    Create SmartWatch.java

---
+++ Java/objeto/SmartWatch.java
@@ -0,0 +1,21 @@
+public class SmartWatch extends SmartDevice{
+    Boolean gps = true;
+
+    public SmartWatch (){
+    }
+
+    public SmartWatch(String marca, String tipo, int anio, boolean gps) {
+        super(marca, tipo, anio);
+        this.gps=gps;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartWatch{" +
+                "gps=" + gps +
+                ", marca='" + marca + '\'' +
+                ", tipo='" + tipo + '\'' +
+                ", anio=" + anio +
+                '}';
+    }
+}
