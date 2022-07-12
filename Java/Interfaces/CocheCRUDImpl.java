d59dc65f5c7986606ea17b50687d40193de5d883
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-07-12 18:39:38 -0300

    Create CocheCRUDImpl.java

---
+++ Java/Interfaces/CocheCRUDImpl.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+import java.util.List;
+
+public class CocheCRUDImpl implements CocheCRUD{
+    @Override
+    public void save(Empleado empleado){
+
+    }
+    @Override
+    public List<Empleado> findAll(){
+        return null;
+    }
+
+    @Override
+    public void delete(Empleado empleado){
+
+    }
+}
