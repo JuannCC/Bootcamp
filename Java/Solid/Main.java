3d9388f762eb3314aacf6cb4fc1ec4bb77f33c6f
JuannCC <90645839+JuannCC@users.noreply.github.com>
2022-08-19 15:20:52 -0300

    Create Main.java

---
+++ Java/Solid/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String []args) {
+        Vehiculo vehiculo = new Vehiculo("Coche");
+
+        VehiculoDB vehiculoDB = new VehiculoDB();
+        vehiculoDB.guardarVehiculo(vehiculo);
+    }
+}
